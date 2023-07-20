dcbfd48d95e3ee2d6d629341b6e7e7371f6064f8
Oleksandr Gneushev <galexing@gmail.com>
2023-07-20 15:16:14 +0200

    Update app.ts

--- Frontend/soniak-bew-front/app.ts
+++ Frontend/soniak-bew-front/app.ts
@@ -1,50 +1,44 @@
-import { Request, Response } from "express";
-import { Client } from "./model/client";
-const express = require("express");
-const path = require("path");
-const nunjucks = require("nunjucks");
-const session = require("express-session");
+import { Request, Response } from "express"
+
+const express = require('express');
+const path = require('path');
+const nunjucks = require('nunjucks');
+const session = require('express-session')
 
 const app = express();
 
-const appViews = path.join(__dirname, "/views/");
+const appViews = path.join(__dirname, '/views/');
 
 const nunjucksConfig = {
-  autoescape: true,
-  noCache: true,
-  express: app,
-};
+    autoescape: true,
+    noCache: true,
+    express: app
+}
 
 nunjucks.configure(appViews, nunjucksConfig);
 
-app.set("view engine", "html");
+app.set('view engine', 'html');
 
-app.use("/public", express.static(path.join(__dirname, "public")));
+//app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.use(express.json());
+app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }));
 
 //app.use(session({ secret:'NOT HARDCODED SECRET', cookie: {  maxAge: 60000 }}));
 
 declare module "express-session" {
-  interface SessionData {
-    // client: Client;
-    token: string;
-  }
+    interface SessionData {
+        token: string;
+    }
 }
 
-app.get("/", (req: Request, res: Response) => {
-  res.render("index", { title: "Soniak Bew" });
-});
-
-require("./controller/authController")(app);
-
-// const authMiddleware = require("./middleware/auth");
-// app.use(authMiddleware);
-
-require("./controller/clientController")(app);
-
 app.listen(3000, () => {
-  console.log("Server listening on port 3000");
-});
+    console.log('Server listening on port 3000');
+})
+
+app.get('/', (req: Request, res: Response) => {
+    res.render('pizza', { title: 'Martyna\'s Pizza Time!' })
+})
+
+//require('./controller/*')(app);
