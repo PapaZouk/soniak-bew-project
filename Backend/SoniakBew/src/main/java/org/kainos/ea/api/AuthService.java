f864c201c6444cd549eb9448f05f6ac6515c12e3
Paweł Skóra <pawel.skora@kainos.com>
2023-07-20 11:26:52 +0200

    feature: Added login() to AuthService and exceptions

--- Backend/SoniakBew/src/main/java/org/kainos/ea/api/AuthService.java
+++ Backend/SoniakBew/src/main/java/org/kainos/ea/api/AuthService.java
@@ -1,4 +1,28 @@
 package org.kainos.ea.api;
 
+import org.kainos.ea.cli.Login;
+import org.kainos.ea.client.FailedToGenerateTokenException;
+import org.kainos.ea.client.FailedToLoginException;
+import org.kainos.ea.db.AuthDao;
+
+import java.sql.SQLException;
+
 public class AuthService {
+    private AuthDao authDao = new AuthDao();
+
+    public String login(Login login) throws FailedToLoginException, FailedToGenerateTokenException {
+
+        try {
+            if (authDao.validLogin(login)) {
+                return authDao.generateToken(login.getUsername());
+            }
+        } catch (SQLException e) {
+            throw new FailedToGenerateTokenException();
+        }
+
+        throw new FailedToLoginException();
+    }
+
+
+
 }
