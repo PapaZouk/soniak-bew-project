6ada2df41f27d738085f9eb29778648fc62345d9
Rafal Papala <rafal.papala@kainos.com>
2023-07-20 22:38:03 +0200

    feat: adding method do add client to the project and fixes some API adnotations

--- Backend/SoniakBew/src/main/java/org/kainos/ea/resources/AuthController.java
+++ Backend/SoniakBew/src/main/java/org/kainos/ea/resources/AuthController.java
@@ -16,7 +16,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import static org.kainos.ea.resources.AuthController.*;
+import static org.kainos.ea.resources.AuthController.API;
 
 @Api
 @Path(API)
