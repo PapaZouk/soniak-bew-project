987c8a7f8716e8aebd1ff582b45baf0161866fc2
Paweł Skóra <pawel.skora@kainos.com>
2023-07-20 11:48:35 +0200

    fix: fixed import

--- Backend/SoniakBew/src/main/java/org/kainos/ea/client/TokenExpiredException.java
+++ Backend/SoniakBew/src/main/java/org/kainos/ea/client/TokenExpiredException.java
@@ -1,7 +1,5 @@
 package org.kainos.ea.client;
 
-import jdk.javadoc.internal.doclets.toolkit.taglets.ThrowsTaglet;
-
 public class TokenExpiredException extends Throwable {
     @Override
     public String getMessage(){
